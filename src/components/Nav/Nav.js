0b644ca8ac60629218b82c4cae799cd0785c8caf
Corie Watson <watson.corie@gmail.com>
2023-02-15 05:32:49 +0000

    Added GitHub link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
         <h1><a href="/">Grade Calculator</a></h1>
         <ul>
             <li><a href="#">Create format</a></li>
-            <li><a href="#">GitHub</a></li>
+            <li><a href="https://github.com/CorieW/Grade-Calculator">GitHub</a></li>
         </ul>
     </div>
   );
